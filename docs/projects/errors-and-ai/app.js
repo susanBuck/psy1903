d4246f4cfbae875b731234c5a5208a0027ab3c6c
Susan Buck <susanbuck@fas.harvard.edu>
2024-09-12 21:43:09 -0400

    Week 3 code from videos

---
+++ docs/projects/errors-and-ai/app.js
@@ -0,0 +1,17 @@
+// app.js:1 Uncaught ReferenceError: hello is not defined
+// console.log(hello);
+
+// Uncaught SyntaxError: missing ) after argument list
+// alert(Welcome to our experiment);
+
+// Uncaught ReferenceError: Welcome is not defined
+// alert(Welcome, to, our, experiment);
+
+// alert('Welcome to our experiment');
+
+// Uncaught SyntaxError: Unexpected identifier 'code'
+//alert([your code here]);
+
+// array 
+// let scores = [99, 100, 75, 62];
+// console.log(scores);
