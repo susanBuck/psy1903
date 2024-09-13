0694042847728ab35a1d0bf8600a9f5c87e7d464
Susan Buck <susanbuck@fas.harvard.edu>
2024-09-13 06:30:42 -0400

    Revert to state from Week 2

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,16 +1,37 @@
-// Create variables to store references to elements on the page
-let form = document.getElementsByTagName('form')[0];
-let results = document.getElementById('results');
+let experiment = "Stroop";
 
-// Listen for the form to be submitted
-form.addEventListener('submit', function (event) {
+let welcomeMessage = `
+Welcome to our ` + experiment + ` experiment! 
+Please read the instructions carefully.
+Welcome to our ` + experiment + ` experiment!
+Please read the instructions carefully.
+Welcome to our ` + experiment + ` experiment!
+Please read the instructions carefully.
+`;
 
-    // Prevent the default form submission b
-    event.preventDefault();
+// console.log(welcomeMessage);
+// console.log(typeof experiment); // string
+// console.log(experiment.charAt(1)); // 't'
 
-    // Collect the response
-    let response = form.elements['response'].value;
+let trialCountMax = 20.5;
+// console.log(typeof trialCountMax); // number
+// console.log(trialCountMax.charAt(0));
 
-    // Report the results
-    results.innerHTML = 'Your response:' + response;
-});
+// TODO: Randomize colors
+let colors = ['red', 'green', 'blue'];
+
+// alert('Welcome to the ' + experiment + ' experiment!');
+
+trialCountMax = 40;
+
+// At the halfway point we will display a pause screen
+let halfWayCount = trialCountMax / 2;
+
+//console.log(halfWayCount); // Expected: 20
+
+let correct = false;
+console.log(typeof correct);
+
+console.log(10 <= 15); // true
+
+
