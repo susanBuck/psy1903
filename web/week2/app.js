6e9ba60b8a733bf9732477e7439f2e4217cfb612
Susan Buck <susanbuck@fas.harvard.edu>
2025-09-04 18:37:41 -0400

    Week 2 practice code from videos

--- web/week2/app.js
+++ web/week2/app.js
@@ -1,1 +1,37 @@
-console.log('Hello World!');
+let experiment = "Stroop";
+
+let welcomeMessage = `
+Welcome to our ` + experiment + ` experiment! 
+Please read the instructions carefully.
+Welcome to our ` + experiment + ` experiment!
+Please read the instructions carefully.
+Welcome to our ` + experiment + ` experiment!
+Please read the instructions carefully.
+`;
+
+// console.log(welcomeMessage);
+// console.log(typeof experiment); // string
+// console.log(experiment.charAt(1)); // 't'
+
+let trialCountMax = 20.5;
+// console.log(typeof trialCountMax); // number
+// console.log(trialCountMax.charAt(0));
+
+// TODO: Randomize colors
+let colors = ['red', 'green', 'blue'];
+
+// alert('Welcome to the ' + experiment + ' experiment!');
+
+trialCountMax = 40;
+
+// At the halfway point we will display a pause screen
+let halfWayCount = trialCountMax / 2;
+
+//console.log(halfWayCount); // Expected: 20
+
+let correct = false;
+console.log(typeof correct);
+
+console.log(10 <= 15); // true
+
+
